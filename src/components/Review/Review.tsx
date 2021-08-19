c41fb438ebc25dd143def88f648054cf59510d5d
Joonas Harjukallio <harjukallio@gmail.com>
2021-08-19 19:16:41 +0300

    Ok

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -6,13 +6,12 @@ import { ReviewHeader } from "../ReviewHeader/ReviewHeader";
 
 export const Review = () => {
   const left = <List items={getLeftSideItems()} />;
-  return <Split left={left} right={<div>right</div>} />;
-  // return (
-  //   <div className={s.container}>
-  //     <ReviewHeader />
-  //     <Split left={left} right={<div>righ</div>} />
-  //   </div>
-  // );
+  return (
+    <div className={s.container}>
+      <ReviewHeader />
+      <Split left={left} right={<div>righ</div>} />
+    </div>
+  );
 };
 
 function getLeftSideItems(num: number = 100) {
