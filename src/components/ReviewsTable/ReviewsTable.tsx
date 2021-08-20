ee1f81dfb6500f1dff6a85c8daf8ee6dba5f540f
Joonas Harjukallio <harjukallio@gmail.com>
2021-08-20 11:48:04 +0300

    Add buttons on top of table

--- src/components/ReviewsTable/ReviewsTable.tsx
+++ src/components/ReviewsTable/ReviewsTable.tsx
@@ -6,6 +6,11 @@ import { Table } from "../Table/Table";
 export const ReviewsTable = () => {
   return (
     <div className={s.container}>
+      <div className={s.buttonContainer}>
+        <button>Add button</button>
+        <button>Remove button</button>
+      </div>
+
       <Table columns={reviewColumns} data={getReviews()} />
     </div>
   );
